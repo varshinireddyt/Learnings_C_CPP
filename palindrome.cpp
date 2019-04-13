94cc926aee2efe59af1bc7f0c8bca1472f4a9c0c
Varshini Thatiparthi <49504863+varshinireddyt@users.noreply.github.com>
2019-04-13 15:45:25 -0500

    Palindrome function
    - Storing the string in a character array.
    - creating a temp character array property and assigning length(no of characters) to initialize that array.
    - reversing the string character and storing in the temp character array.
    - creating a `flag` and assigning it to `true`.
    -if character array and temp char array is same then i am not doing anything.
    - If character array is not matching then assigning flag as false. Which means it is not a palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  reverse
+//
+//  Created by Srikanth Adavalli on 4/12/19.
+//  Copyright © 2019 Varshini Thatiparthi. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstdio>
+using namespace std;
+
+// reverse of string and validating is palindrome or not.
+int main(int argc, const char * argv[]) {
+
+    char char_string[] = "bob";
+    char dup_string[strlen(char_string)];
+
+    for(long i = strlen(char_string) - 1; i >= 0; i--) {
+        dup_string[strlen(char_string) - i -1] = char_string[i];
+    }
+
+    bool flag = true;
+    for(long i = 0; i < strlen(char_string); i++) {
+        if(char_string[i] == dup_string[i]) {
+        } else {
+            flag = false;
+        }
+    }
+
+    cout << "is palindrome : ";
+    if (flag) {
+        printf("true");
+    } else {
+        printf("false");
+    }
+
+    printf("\n");
+    return 0;
+}
